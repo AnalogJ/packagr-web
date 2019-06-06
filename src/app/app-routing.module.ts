bfeae587a4e6e9640314476c414e3836f4dd2043
Jason Kulatunga <jason@thesparktree.com>
2019-06-05 21:31:30 -0700

    add support for the beta branch.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AuthConnectComponent} from './auth-connect/auth-connect.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AuthCallbackComponent} from './auth-callback/auth-callback.component';
 
 const routes: Routes = [
   { path: 'login', component: AuthConnectComponent },
+  { path: 'auth/connect', component: AuthConnectComponent },
+  { path: 'auth/callback/:serviceType', component: AuthCallbackComponent },
+
+  { path: 'dashboard', component: DashboardComponent },
   { path: '**', redirectTo: 'login' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthConnectComponent,
-    AuthCallbackComponent
+    AuthCallbackComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
