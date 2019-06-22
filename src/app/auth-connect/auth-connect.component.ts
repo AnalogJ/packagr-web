acad411122ada67e7cb19ee336ec23c3a8d28ad1
Jason Kulatunga <jason@thesparktree.com>
2019-06-21 22:57:16 -0700

    added api services and token managment.

--- src/app/auth-connect/auth-connect.component.ts
+++ src/app/auth-connect/auth-connect.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-auth-connect',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthConnectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  loading = {
+    gh: false,
+    bb: false
+  };
 
   ngOnInit() {
   }
 
+  loginWith(serviceType){
+    console.log(serviceType);
+    this.loading[serviceType] = true;
+
+    this.apiService.authConnect(serviceType)
+      .subscribe(
+        data => {
+          console.log(data);
+          window.location.href = data.url;
+        },
+        error => console.log(error),
+        () => this.loading[serviceType] = false
+      );
+  }
 }
